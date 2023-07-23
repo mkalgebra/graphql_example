f41fd41a33332531a907611c6b693c0ea50784e0
Marijo Kis <marijo.kis@q.agency>
2023-07-23 12:46:33 +0200

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: 'https://spacex-production.up.railway.app/',
+});
+
+function App(){
+  const [data, setData] = useState()
+
+  async function getData() {
+    const data = await client
+    .query({
+      query: gql`
+      query($limit: Int) {
+          launchesPast(limit: $limit) {
+              id
+              mission_name
+              rocket {
+              rocket_name
+              }
+              upcoming
+              launch_date_unix
+          }
+      }
+  `,
+  fetchPolicy: "network-only",
+    variables: {
+        limit: 10,
+    }
+    })
+    .then((result) => result)
+
+    return data
+  }
+
+  async function handleClick(){
+      const res = await getData()
+      setData(res.data.launchesPast)
+  }
+
+  return(<>
+    <div>
+      <button onClick={handleClick}>Get data</button>
+      <ul>
+        {data?.length && data.map(i=>{return(
+          <li key={i.id}>{i.mission_name}</li>
+          )})}
+      </ul>
     </div>
-  );
-}
+  </>)
+};
 
 export default App;
